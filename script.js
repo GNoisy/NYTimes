8f4951d84955b00b0a736002d275fe34f20649e2
Sham <shambhawi.kumari1393@gmail.com>
2020-06-26 16:29:48 -0700

    ajax call to get response added

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log('I am here');
+$(document).ready(function() {
+
+    var articleName;
+    var editor;
+    var section;
+    var datePub;
+    var link;
+    var articleCount;
+    var key = 'iS6ymDj7QSieYhjaWjDXRqmsjtTIt1LS';
+    var year = 2015;
+
+    // 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + key;
+    // fq=pub_year:("2015" "2016")
+
+    var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + key + '&fq=pub_year:(' + year + ')';
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var results = response.response.docs;
+        articleCount = $('#article-count')? $('#article-count'): 10;
+        for(let i=0 ; i<articleCount.length; i++){
+            articleName = results[i].headline.main;
+            editor = results[i].byline.original;
+            section = results[i].section_name;
+            datePub = results[i].pub_date;
+            var articleSection = $('#article-section');
+            var articleNameEl = $('<p>').text(articleName);
+            articleSection.append(articleNameEl);
+
+            var editorEl = $('<p>').text(editor);
+            articleSection.append(editorEl);
+
+            var sectionEl = $('<p>').text(section);
+            articleSection.append(sectionEl);
+
+            var dateEl = $('<p>').text(datePub);
+            articleSection.append(dateEl);
+        }
+    })
+});
